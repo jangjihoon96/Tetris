16c5f8011d3d0ce7261dd2631af0f500bfd68f66
Jihoon <jangna96@gmail.com>
2023-07-11 17:42:31 +0900

    feat: 블럭 이동 이벤트 및 함수 제작

--- src/js/index.js
+++ src/js/index.js
@@ -14,7 +14,7 @@ let tempMovingItem;
 const BLOCKS = {
   tree: [
     [
-      [0, 0],
+      [2, 1],
       [0, 1],
       [1, 0],
       [1, 1],
@@ -29,7 +29,7 @@ const movingItem = {
   type: "tree",
   direction: 0,
   top: 0,
-  left: 0,
+  left: 3,
 };
 
 init();
@@ -39,7 +39,7 @@ function init() {
   for (let i = 0; i < GAME_ROWS; i++) {
     prependNewLine();
   }
-  randomBlocks();
+  renderBlocks();
 }
 
 function prependNewLine() {
@@ -53,12 +53,67 @@ function prependNewLine() {
   playground.insertAdjacentElement("afterbegin", li);
 }
 
-function randomBlocks() {
+function renderBlocks() {
   const { type, direction, top, left } = tempMovingItem;
-  BLOCKS[type][direction].forEach((block) => {
-    const x = block[0];
-    const y = block[1];
-    const target = playground.childNodes[y].childNodes[0].childNodes[x];
-    target.classList.add(type);
+  const movingBlocks = document.querySelectorAll(".moving");
+  movingBlocks.forEach((moving) => {
+    moving.classList.remove(type, "moving");
   });
+  BLOCKS[type][direction].forEach((block) => {
+    const x = block[0] + left;
+    const y = block[1] + top;
+    const target = playground.childNodes[y]
+      ? playground.childNodes[y].childNodes[0].childNodes[x]
+      : null;
+    const isAvailable = checkEmpty(target);
+    if (isAvailable) {
+      target.classList.add(type, "moving");
+    } else {
+      tempMovingItem = { ...movingItem };
+      setTimeout(() => {
+        renderBlocks();
+        if (moveType === "top") {
+          seizeBlock();
+        }
+      }, 0);
+      // renderBlocks();
+    }
+  });
+  movingItem.left = left;
+  movingItem.top = top;
+  movingItem.direction = direction;
 }
+
+function seizeBlock() {
+  console.log("seize block");
+}
+
+function checkEmpty(target) {
+  if (!target) {
+    return false;
+  }
+  return true;
+}
+
+function moveBlock(moveType, amount) {
+  tempMovingItem[moveType] += amount;
+  renderBlocks();
+}
+
+// event handling
+document.addEventListener("keydown", (e) => {
+  switch (e.keyCode) {
+    case 39:
+      moveBlock("left", 1);
+      break;
+    case 37:
+      moveBlock("left", -1);
+      break;
+    case 40:
+      moveBlock("top", 1);
+      break;
+    default:
+      break;
+  }
+  console.log(e);
+});
